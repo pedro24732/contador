c2ed503181c2d76233a2c03cd8d35a3723a1a907
Greenx <112995803+pedro24732@users.noreply.github.com>
2025-08-16 10:29:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 function atualizarContador(){
   let agora = new Date();
-  let futuro = new Date("2025 - 09 - 07100:00:00");
+  let futuro = new Date("2025-09-07T00:00:00");
   let tempoRestante = futuro - agora;
   
   if(tempoRestante > 0){
     let dias = Math.floor(tempoRestante/(1000*60*60*24));
      
-    let horas =  Math.floor(tempoRestante/(1000*60*60))%24);
+    let horas =  Math.floor((tempoRestante/(1000*60*60))%24);
     
-    let minutos = Math.floor(tempoRestante/(1000*60))%60);
+    let minutos = Math.floor((tempoRestante/(1000*60))%60);
     
-    let segundos = Math.floor(tempoRestante/(1000))%60);
+    let segundos = Math.floor((tempoRestante/1000)%60);
     
-    document.getElementById("contador").textContent = `${dias}d:${horas}h:${minutos}m:${segundos};`
+    document.getElementById("contador").textContent = `${dias}d:${horas}h:${minutos}m:${segundos}`;
     
   } else {
-    document.getElementById("contador")
+    document.getElementById("contador").textContent = "feliz aniversario";
+    
   }
   
 }
+
+setInterval(atualizarContador, 1000)
+atualizarContador();
